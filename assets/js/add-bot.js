a19e8058d54246aad4de396bed5cf248c83255ab
epell-development <141233682+epell-development@users.noreply.github.com>
2024-04-27 12:31:36 +0000

    fixed bot invite + support discord links

---
+++ assets/js/add-bot.js
@@ -0,0 +1,6 @@
+// Redirect
+var discordURL = "https://discord.com/oauth2/authorize?client_id=1131938421393342554&permissions=1374389660672s&scope=bot%20applications.commands";
+
+function botInvite() {
+    popupWindow = window.open("https://discord.com/oauth2/authorize?client_id=1131938421393342554&permissions=1374389660672s&scope=bot%20applications.commands", 'popUpWindow', 'height=500,width=500,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');
+}
